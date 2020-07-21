be31f5a8ff71b8be67f451a6f2ce68372c2cc755
CARTOGIS33 <61816683+CARTOGIS33@users.noreply.github.com>
2020-07-21 05:26:37 +0200

    Add files via upload

---
+++ data/Ma_Societe_7.js
@@ -0,0 +1,8 @@
+var json_Ma_Societe_7 = {
+"type": "FeatureCollection",
+"name": "Ma_Societe_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Societe": "Ma_Société" }, "geometry": { "type": "Point", "coordinates": [ -0.68942012422937, 44.782405190254408 ] } }
+]
+}
---
+++ data/Portails_6.js
@@ -0,0 +1,21 @@
+var json_Portails_6 = {
+"type": "FeatureCollection",
+"name": "Portails_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Portails": "P1" }, "geometry": { "type": "Point", "coordinates": [ -0.731944960743795, 44.75841788409911 ] } },
+{ "type": "Feature", "properties": { "Portails": "P2" }, "geometry": { "type": "Point", "coordinates": [ -0.732235122822276, 44.7573149631001 ] } },
+{ "type": "Feature", "properties": { "Portails": "P3" }, "geometry": { "type": "Point", "coordinates": [ -0.727552372748273, 44.758279941846723 ] } },
+{ "type": "Feature", "properties": { "Portails": "P4" }, "geometry": { "type": "Point", "coordinates": [ -0.728172079027116, 44.760010963166827 ] } },
+{ "type": "Feature", "properties": { "Portails": "P5" }, "geometry": { "type": "Point", "coordinates": [ -0.725805649064899, 44.759145874102018 ] } },
+{ "type": "Feature", "properties": { "Portails": "P6" }, "geometry": { "type": "Point", "coordinates": [ -0.727146131763055, 44.758387010310216 ] } },
+{ "type": "Feature", "properties": { "Portails": "P7" }, "geometry": { "type": "Point", "coordinates": [ -0.724187647924332, 44.759549353439915 ] } },
+{ "type": "Feature", "properties": { "Portails": "P8" }, "geometry": { "type": "Point", "coordinates": [ -0.725020585772416, 44.76022294357891 ] } },
+{ "type": "Feature", "properties": { "Portails": "P9" }, "geometry": { "type": "Point", "coordinates": [ -0.726943367205232, 44.759264516105624 ] } },
+{ "type": "Feature", "properties": { "Portails": "P10" }, "geometry": { "type": "Point", "coordinates": [ -0.725998461754012, 44.761045972058035 ] } },
+{ "type": "Feature", "properties": { "Portails": "P11" }, "geometry": { "type": "Point", "coordinates": [ -0.728247442287926, 44.760492105194416 ] } },
+{ "type": "Feature", "properties": { "Portails": "P12" }, "geometry": { "type": "Point", "coordinates": [ -0.72894356974452, 44.760379902383114 ] } },
+{ "type": "Feature", "properties": { "Portails": "P13" }, "geometry": { "type": "Point", "coordinates": [ -0.72889263119396, 44.761760576051906 ] } },
+{ "type": "Feature", "properties": { "Portails": "P14" }, "geometry": { "type": "Point", "coordinates": [ -0.729459903348935, 44.761296786750222 ] } }
+]
+}
